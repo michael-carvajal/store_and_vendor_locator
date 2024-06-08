f0abc738bbdbe2e2b714f39a939cb5cc86558c88
Michael Carvajal <michaelcarvajal.work@gmail.com>
2024-06-08 14:05:12 -0400

    all all store markers to the map

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Initialize the map
-var map = L.map("map").setView([37.7749, -122.4194], 13); // Default to San Francisco
+var map = L.map("map").setView([41.214370549784554, -73.71971866138134], 13); // Default to San Francisco
 
 // Add OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -9,17 +9,35 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 var popup = L.popup();
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
 }
 
-map.on('click', onMapClick);
+map.on("click", onMapClick);
 // Store and vendor data
 var stores = [
-  { storeNumber: "001", name: "Store 1", location: [37.7749, -122.4194] },
-  { storeNumber: "002", name: "Store 2", location: [34.0522, -118.2437] },
+  {
+    storeNumber: "001",
+    name: "Store 1",
+    location: [41.214370549784554, -73.71971866138134],
+  },
+  {
+    storeNumber: "002",
+    name: "Store 2",
+    location: [40.95249237435335, -73.8162849767375],
+  },
+  {
+    storeNumber: "003",
+    name: "Store 3",
+    location: [40.9087151880907, -73.84732219023248],
+  },
+  {
+    storeNumber: "004",
+    name: "Store 4",
+    location: [41.303084615923886, -73.88011559090648],
+  },
   // Add more stores as needed
 ];
 
@@ -28,6 +46,12 @@ var vendors = [
   { name: "Vendor 2", location: [34.0522, -118.2437] },
   // Add more vendors as needed
 ];
+// Add all store markers to the map
+stores.forEach((store) => {
+  L.marker(store.location)
+    .addTo(map)
+    .bindPopup(`<b>${store.name}</b><br>Store Number: ${store.storeNumber}`);
+});
 
 // Search and display functionality
 document.getElementById("searchButton").addEventListener("click", function () {
